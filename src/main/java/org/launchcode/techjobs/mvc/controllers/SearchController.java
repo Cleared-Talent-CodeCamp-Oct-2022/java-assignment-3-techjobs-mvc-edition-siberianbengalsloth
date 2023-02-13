964bc3ec17706199434e2749107da66feb2c490a
dovmfdyd3@gmail.com <tp9m96Nd@fXty8xhPhu3!>
2023-02-14 07:57:41 +0900

    Completed the assignment, except for the bonus missions

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,14 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -30,14 +35,23 @@ no need to use annotation as indicated by #4 in the instruction, but need approp
  what html view should this method render?
 
  model.addAttribute("columns", columnChoices); this needs to be in the method if we are going to render it to /search
-
  */
 
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchTerm.equalsIgnoreCase("all") || searchTerm.equals("")) {
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
 
-
-    // @RequestMapping("path??")
-
-
+        return "search";
+    }
 
 
 }
